73b00f3110e9605b734863d2f5803fd09b056e9c
VishalAmbhore2954 <vishalambhore2954@gmail.com>
2025-01-08 20:54:26 +0530

    Add

--- arrUniqueEle.c
+++ arrUniqueEle.c
@@ -1,27 +1,25 @@
 #include<stdio.h>
 int main(){
-	 int a[100],n,i,j,flag,key;
+    int arr[100],n,i,j,k;
     printf("Enter a N : ");
     scanf("%d",&n);
 
     for(i=0;i<n;i++)
-        scanf("%d",&a[i]);
-	
-	for(i=0;i<n;i++){
-		if(a[i]==a[i+1]){
-			for(j=i+1;j<n;j++){
-				a[j]=a[j+1];
-				n--;
-				j--;
-		}
-		i--;
-		}
-		
-	}
-	
+        scanf("%d",&arr[i]);
 
-    printf("Array elements is : ");
+    for(k=0;k<n;k++){
+    	for(i=k+1;i<n;i++){
+        if(arr[k]==arr[i]){
+            for(j=i;j<n-1;j++)
+                arr[j]=arr[j+1];
+            n--;
+            j--;
+            i--;
+        }
+    }
+	}
+
+    printf("Array element is : ");
     for(i=0;i<n;i++)
-    	printf("%d ",a[i]);
-	return 0;
+        printf("%d ",arr[i]);
 }
